3f9b61f661a0b2fa462993ec9997b4973c285ae3
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2022-12-23 03:00:21 +0530

    small changes

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -18,9 +18,10 @@ function Header() {
       });
   return (
     <div className='bg-purple-400 flex flex-col lg:flex-row items-center justify-center gap-10  p-4 pb-2'>
-                    <div className='text-white font-semibold lg:text-[1rem]  text-[1rem] float-left'>
-                    Phone: +91 9398695177/ 9550001512
-                        Mail: @leocarefoundation.gmail.com
+                    <div className='text-white font-semibold lg:text-[1rem]  text-[1rem] float-left grid sm:grid-cols-1 lg:grid-cols-2'>
+                    <div>Phone: +91 9398695177/ 9550001512</div>
+                    <div>Mail: @leocarefoundation.gmail.com</div>
+                        
                         
                     </div>
                     <form id="rzp_payment_form"></form>
