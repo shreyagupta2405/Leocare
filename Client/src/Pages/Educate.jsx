b1dac58ff2d9f7256e09efa4937918758de5ec5a
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2022-12-24 02:55:51 +0530

    educate improved

--- Client/src/Pages/Educate.jsx
+++ Client/src/Pages/Educate.jsx
@@ -1,37 +1,35 @@
 import React from 'react'
 import Blogs from '../Components/Blogs';
+import img from '../Components/images/kid.png'
 
 function Educate() {
   return (
     <div>
-     <div className='p-6 text-3xl text-primary flex flex-col justify-center items-center'>
-    Educate a Child
-    </div>
-      
+      <div className='p-6 text-4xl text-gray-700 flex flex-col justify-center items-center'>
+        Educate a Child
+      </div>
+
       <Blogs />
-      
 
-      <div className='mt-40 place-items-center flex items-center justify-center'>
-      <div className='bg-purple-400 p-8 text-white w-2/4 text-center'>
-      <div className='text-3xl p-4 m-2'>Contact Information</div>
-      
-      <div className='p-4'>
-      <div className='text-lg text-black bg-white w-auto p-2 rounded-lg '>Mobile:<br /> 9398695177 <br />   9550001512 <br /> </div>
-      <div className='bg-white m-4 p-3 rounded-lg w-auto 
-      flex items-center justify-center text-black text-lg'>Email: leocarefoundation@gmail.com</div>
-      </div>
-      
-      <div>© Leocare 2022</div>
-       </div>
-
-
+      <div className='shadow-xl my-44 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 '>
+        <div className='bg-white p-4 font-serif text-center text-gray-700'>
+          <div className='text-5xl mt-16'>Contact Information</div>
+          <div className='p-4 text-2xl grid place-items-center mt-16'>
+            <div>Mobile: <br></br> +91 9398695177 / 9550001512 </div>
+            <div><br />Email: <br />leocarefoundation@gmail.com</div>
+          </div>
+          <div className='text-sm mt-28 font-sans bottom-0 left-0'> © 2022 Leocare</div>
+        </div>
+        <div className='text-center'>
+        <img src={img} className=' w-full'></img>
+        </div>
       </div>
 
-      
+
     </div>
   )
 }
 
 export default Educate;
 //9398695177
-//9550001512
+//9550001512 Email: leocarefoundation@gmail.com
