fdc3b79e0851a9afe885d8e751bb8c27a7bf4601
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2022-12-26 17:41:54 +0530

    header links added

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 function Navbar() {
 
     return (
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 drop-shadow-lg ">
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-3 rounded dark:bg-gray-900 drop-shadow-lg ">
             <div className=" flex flex-wrap items-center justify-between mx-auto ">
                 <a className="flex items-center">
                     <img src={im} className="h-13 w-14 mr-2 "  />
@@ -17,12 +17,11 @@ function Navbar() {
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex flex-col  mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 items-center">
                         <li>
                             <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
                         </li>
                         <li>
-                            
                             <AnchorLink href='#info' className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</AnchorLink>
                         </li>
                         <li>
@@ -35,13 +34,19 @@ function Navbar() {
                         <li>
                             <a href="/articles" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Articles</a>
                         </li>
-                        <li>
+                        {/* <li>
                             <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Volunteer</a>
-                        </li>
+                        </li> */}
                         <li>
                             <AnchorLink href="#footer" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</AnchorLink>
                         </li>
+                        <li>
+                        <button type="button" className=" float-right text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-small rounded-lg text-sm px-5 py-2 text-center ">
+        <a href='https://pages.razorpay.com/leocarefoundation' target="_blank">DONATE</a> 
+        </button>
+                        </li>
                     </ul>
+                    
                 </div>
             </div>
         </nav>
