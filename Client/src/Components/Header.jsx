b8bbaac6d70c4244755074d1781c8274e7e9cc15
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2023-01-10 01:15:08 +0530

    Update Header.jsx

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -4,18 +4,18 @@ import { BsFacebook } from 'react-icons/bs';
 import { BsTwitter } from 'react-icons/bs';
 import { BsInstagram } from 'react-icons/bs';
 import { BsLinkedin } from 'react-icons/bs';
-import {MdEmail} from 'react-icons/md'
-import {BsFillTelephoneFill} from 'react-icons/bs'
+import { MdEmail } from 'react-icons/md'
+import { BsFillTelephoneFill } from 'react-icons/bs'
 // import { useEffect } from 'react';
 
 function Header() {
   return (
-    <div className='justify-between bg-purple-500 flex flex-col lg:flex-row gap-2 lg:gap-10 p-2 '>
+    <div className='justify-between bg-purple-400 flex flex-col lg:flex-row gap-2 lg:gap-10 p-2 '>
       <div className='ml-4 text-white font-semibold lg:text-[1rem] text-[1rem] p-2 grid sm:grid-cols-1 lg:grid-cols-2'>
-        <div className='flex items-center justify-center flex-row sm:px-2 lg:px-1'><BsFillTelephoneFill size={25}/> +91 9398695177/ 9550001512</div>
+        <div className='flex items-center justify-center flex-row sm:px-2 lg:px-1'><BsFillTelephoneFill size={25} /> +91 9398695177/ 9550001512</div>
         <div className='flex items-center justify-center flex-row sm:px-2 lg:px-1'>
-        <MdEmail size={25}/> @leocarefoundation.gmail.com</div>
-        
+          <MdEmail size={25} /> @leocarefoundation.gmail.com</div>
+
       </div>
       {/* <Donate /> */}
       <div className='mb-2 lg:mb-0 lg:mr-8 flex justify-center gap-8 lg:justify-end'>
