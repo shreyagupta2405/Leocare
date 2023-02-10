929321d4903d37dbf900b96a1b98d7bc03c72b9f
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2023-02-10 20:57:49 +0530

    changes made lets see

--- Client/src/Components/Journey.jsx
+++ Client/src/Components/Journey.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import children from './images/children.jpeg'
 
 function Journey() {
-  return (
-    <div>
-      The journey to change the world starts with one step.
-    </div>
-  )
+    return (
+        <div className='flex'>
+            <div className='p-8'>
+                {/* <div className='flex py-24 px-8 gap-8'>
+                    <img className='h-64 w-64' src={children}></img>
+                    <img className='h-64 w-64' src={children} ></img>
+                </div> */}
+            </div>
+            <div className='p-8 flex flex-row'>
+                <div className='font-bold text-xl p-4'>The journey to change the world starts with one step.</div>
+                <div className='p-4'>Sadly, not every one is in a positive environment. The Leocare Foundation is committed to helping the most vulnerable women, children and elderly by providing them with protection, education, and support.</div>
+                <div className='p-4'>We are committed to transforming their lives because we know that it will change the world we live in.</div>
+            </div>
+        </div>
+    )
 }
 
 export default Journey
--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -293,7 +293,144 @@ function Navbar() {
                                                                 />
                                                             )}
 
-                                                            <AnchorLink href="#whatWeDo" className="hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"> What We Do</AnchorLink>
+                                                            <div className="top-16  text-right">
+
+
+
+                                <Menu as="div" className=" text-left">
+                                    <div>
+                                        <Menu.Button className="inline-flex w-full justify-center rounded-md text-sm font-medium text-black focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                                            What We Do
+                                            <ChevronDownIcon
+                                                className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
+                                                // aria-hidden="true"
+                                            />
+                                        </Menu.Button>
+                                    </div>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-100"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="absolute left-56  w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <div className="px-1 py-1 ">
+                                                
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <button
+                                                            className={`${active ? 'bg-violet-500 ' : 'text-gray-900'
+                                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        >
+                                                            {active ? (
+                                                                <MoveActiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            ) : (
+                                                                <MoveInactiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            )}
+                                                            <a className='hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href='/empowerment'>Women Empowerment</a>
+                                                        </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <button
+                                                            className={`${active ? 'bg-violet-500 ' : 'text-gray-900'
+                                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        >
+                                                            {active ? (
+                                                                <MoveActiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            ) : (
+                                                                <MoveInactiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            )}
+                                                            <a className='hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href='/educate'>Educate</a>
+                                                        </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <button
+                                                            className={`${active ? 'bg-violet-500 ' : 'text-gray-900'
+                                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        >
+                                                            {active ? (
+                                                                <MoveActiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            ) : (
+                                                                <MoveInactiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            )}
+                                                            <a className='hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href='/training'>Skill Development</a>
+                                                        </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <button
+                                                            className={`${active ? 'bg-violet-500 ' : 'text-gray-900'
+                                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        >
+                                                            {active ? (
+                                                                <MoveActiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            ) : (
+                                                                <MoveInactiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            )}
+                                                            <a className='hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href='/elderlyCare'>Elderly Care</a>
+                                                        </button>
+                                                    )}
+                                                </Menu.Item>
+                                                <Menu.Item>
+                                                    {({ active }) => (
+                                                        <button
+                                                            className={`${active ? 'bg-violet-500 ' : 'text-gray-900'
+                                                                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                                        >
+                                                            {active ? (
+                                                                <MoveActiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            ) : (
+                                                                <MoveInactiveIcon
+                                                                    className="mr-2 h-5 w-5"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            )}
+                                                            <a className='hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' href='/env' >Environment</a>
+                                                        </button>
+                                                    )}
+                                                </Menu.Item>
+                                            </div>
+
+
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
                                                         </button>
                                                     )}
                                                 </Menu.Item>
