5e1d182ebc349ffc9b88510f9f82df41ba5e020b
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2023-02-12 11:35:14 +0530

    text-black

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -380,7 +380,7 @@ function Navbar() {
                                                                     aria-hidden="true"
                                                                 />
                                                             )}
-                                                            <AnchorLink href="#contactus" className="hover:scale-105 block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</AnchorLink>
+                                                            <AnchorLink href="#contactus" className="hover:scale-105 block py-2 pl-3 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</AnchorLink>
                                                         </button>
                                                     )}
                                                 </Menu.Item>
