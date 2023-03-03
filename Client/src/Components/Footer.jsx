81e283638ebf58006f80418ad1be409f2e842a84
Shreya Gupta <90089455+shreyagupta2405@users.noreply.github.com>
2023-03-03 20:13:46 +0530

    footer colour updated

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -17,22 +17,22 @@ import { IoMdContact } from 'react-icons/io'
 function Footer() {
     return (
         <footer id='footer' aria-label="Site Footer" class="bg-white">
-            <div className=''>
+            <div className='bg-gray-500 text-white'>
                 
 
                 <div id='contactus' class=" mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-32">
                     <div class="max-w-sm lg:max-w-none mt-24 px-16">
-                        <p class=" mt-4 lg:ml-24 text-center text-gray-500 lg:text-left lg:text-lg ">
-                            <div className='text-center'>Keep up with our events on social media </div>
+                        <p class=" mt-4 lg:ml-24 text-center lg:text-left lg:text-lg ">
+                            <div className='text-center text-white'>Keep up with our events on social media </div>
                             <div className='flex flex-row gap-2 text-center justify-center items-center'><MdEmail className='mt-1' size={23} />support@leocare.org</div>
-                            <div className='text-black items-center justify-center mt-3 font-extrabold flex flex-row'><IoMdContact className=' text-gray-700 transition hover:text-gray-700/75' size={20}/> Contact</div>
+                            <div className='text-white items-center justify-center mt-3 font-extrabold flex flex-row'><IoMdContact className=' text-white transition hover:text-white' size={20}/> Contact</div>
                         </p>
 
                         
                         <div class="mt-1 lg:ml-24 flex  gap-6 justify-center items-center">
                         
                             <a
-                                className="flex justify-center items-center text-gray-700 transition hover:text-gray-700/75"
+                                className="flex justify-center items-center text-white transition hover:text-gray-700/75"
                                 href="https://www.facebook.com/leocarefoundation"
                                 target="_blank"
                                 rel="noreferrer"
@@ -41,7 +41,7 @@ function Footer() {
                             </a>
 
                             <a
-                                className="flex justify-center items-center text-gray-700 transition hover:text-gray-700/75"
+                                className="flex justify-center items-center text-white transition hover:text-gray-700/75"
                                 href="https://www.linkedin.com/in/udpl72/"
                                 target="_blank"
                                 rel="noreferrer"
@@ -50,7 +50,7 @@ function Footer() {
                             </a>
 
                             <a
-                                className="flex justify-center items-center text-gray-700 transition hover:text-gray-700/75"
+                                className="flex justify-center items-center text-white transition hover:text-gray-700/75"
                                 href="https://www.instagram.com/leocarefoundation/"
                                 target="_blank"
                                 rel="noreferrer"
@@ -59,14 +59,14 @@ function Footer() {
                             </a>
 
                             <a
-                                className="flex justify-center items-center text-gray-700 transition hover:text-gray-700/75"
+                                className="flex justify-center items-center text-white transition hover:text-gray-700/75"
                                 href="https://wa.me/9550001512"
                                 target="_blank"
                                 rel="noreferrer">
                                 <IoLogoWhatsapp size={29} />
                             </a>
                             <a
-                                className="flex justify-center items-center text-gray-700 transition hover:text-gray-700/75"
+                                className="flex justify-center items-center text-white transition hover:text-gray-700/75"
                                 href=""
                                 target="_blank"
                                 rel="noreferrer"
@@ -74,7 +74,7 @@ function Footer() {
                                 <BsYoutube className='mt-1' size={30} />
                             </a>
                             <a
-                                className="flex justify-center items-center text-gray-700 transition hover:text-gray-800/75"
+                                className="flex justify-center items-center text-white transition hover:text-gray-800/75"
                                 href="/docs"
                                 target=""
                                 rel="noreferrer">
@@ -92,35 +92,35 @@ function Footer() {
 
 
                         <div>
-                            <strong class="items-center justify-center flex flex-row gap-2 text-lg text-gray-900 mt-4">
+                            <strong class="items-center justify-center flex flex-row gap-2 text-lg text-white mt-4">
                                 <AiFillHome size={24} /> Location </strong>
 
                             <nav
                                 aria-label="Footer About Nav"
                                 class="mt-2 flex flex-col space-y-1"
                             >
-                                <div class="text-center text-gray-700 transition " >
+                                <div class="text-center text-white transition " >
                                     Libra avenue, Nadergul, Hyderabad,
                                 </div>
-                                <div class="text-center text-gray-700 transition " >
+                                <div class="text-center text-white0 transition " >
                                     Telangana, India. 501510
                                 </div>
                             </nav>
                         </div>
 
                         <div>
-                            <strong class="flex flex-row items-center justify-center gap-2 text-lg text-gray-900 mt-4">
+                            <strong class="flex flex-row items-center justify-center gap-2 text-lg text-white mt-4">
                                 <BsFillTelephonePlusFill className='mt-1' size={22} /> Mobile </strong>
 
                             <nav
                                 aria-label="Footer Support Nav"
                                 class="mt-6 flex flex-col space-y-1"
                             >
-                                <div class="items-center justify-center flex text-gray-700 transition ">
+                                <div class="items-center justify-center flex text-white transition ">
                                     +91 9398695177
                                 </div>
 
-                                <div class="items-center justify-center flex text-gray-700 transition ">
+                                <div class="items-center justify-center flex text-white transition ">
                                     +91 9550001512
                                 </div>
                             </nav>
@@ -129,7 +129,7 @@ function Footer() {
                 </div>
 
                 <div class="mt-16 border-t border-gray-100 pt-8">
-                    <p class="text-center text-xs leading-relaxed text-gray-500 gap-4 flex flex-row justify-center items-center">
+                    <p class="text-center text-xs leading-relaxed text-white gap-4 flex flex-row justify-center items-center">
                         <div>© 2022 Leocare Foundation</div> <a href='https://drive.google.com/file/d/1Km8Xd1OWWAE6gLLDSDmXljXUpxNeDn6J/view' target='_blank'>Privacy Policy</a> <div>All rights reserved.</div>
                     </p>
                 </div>
